567a42b169d29e2191ec280b57c2a10a2c1cf8f5
The chicken  MAN <856030@dpsk12.net>
2025-05-02 16:45:22 -0600

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://blue898789.vercel.app";//your website url
+  let target = "https://vtx.onrender.com";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
