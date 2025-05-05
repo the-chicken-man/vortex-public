0dc8b5cdd4a75e8234c0bbfb8a23a023f4a04df1
The chicken  MAN <856030@dpsk12.net>
2025-05-05 10:31:59 -0600

    cool commit

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://vtx.onrender.com";//your website url
+  let target = "https://sites.google.com/view/drive-u-7-home/";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
